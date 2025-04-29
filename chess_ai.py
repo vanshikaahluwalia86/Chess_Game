9043ae93e5d3ec0d6342b11712c4d5f85cfd1ade
vanshikaahluwalia86 <vanshikaahluwalia1111@gmail.com>
2025-04-29 17:30:07 +0530

    Update chess_ai.py

--- chess_ai.py
+++ chess_ai.py
@@ -4,7 +4,7 @@ import math
 
 
 class ChessAI:
-    def __init__(self, depth=3):
+    def __init__(self, depth=4):
         self.depth = depth
         self.piece_values = {
             chess.PAWN: 100,
@@ -14,62 +14,114 @@ class ChessAI:
             chess.QUEEN: 900,
             chess.KING: 20000
         }
+        self.piece_square_tables = {
+            chess.PAWN: [
+                0, 0, 0, 0, 0, 0, 0, 0,
+                5, 10, 10, -20, -20, 10, 10, 5,
+                5, -5, -10, 0, 0, -10, -5, 5,
+                0, 0, 0, 20, 20, 0, 0, 0,
+                5, 5, 10, 25, 25, 10, 5, 5,
+                10, 10, 20, 30, 30, 20, 10, 10,
+                50, 50, 50, 50, 50, 50, 50, 50,
+                0, 0, 0, 0, 0, 0, 0, 0
+            ],
+            chess.KNIGHT: [
+                -50, -40, -30, -30, -30, -30, -40, -50,
+                -40, -20, 0, 0, 0, 0, -20, -40,
+                -30, 0, 10, 15, 15, 10, 0, -30,
+                -30, 5, 15, 20, 20, 15, 5, -30,
+                -30, 0, 15, 20, 20, 15, 0, -30,
+                -30, 5, 10, 15, 15, 10, 5, -30,
+                -40, -20, 0, 5, 5, 0, -20, -40,
+                -50, -40, -30, -30, -30, -30, -40, -50
+            ],
+            chess.BISHOP: [
+                -20, -10, -10, -10, -10, -10, -10, -20,
+                -10, 0, 0, 0, 0, 0, 0, -10,
+                -10, 0, 5, 10, 10, 5, 0, -10,
+                -10, 5, 5, 10, 10, 5, 5, -10,
+                -10, 0, 10, 10, 10, 10, 0, -10,
+                -10, 10, 10, 10, 10, 10, 10, -10,
+                -10, 5, 0, 0, 0, 0, 5, -10,
+                -20, -10, -10, -10, -10, -10, -10, -20
+            ],
+            chess.ROOK: [
+                0, 0, 0, 0, 0, 0, 0, 0,
+                5, 10, 10, 10, 10, 10, 10, 5,
+                -5, 0, 0, 0, 0, 0, 0, -5,
+                -5, 0, 0, 0, 0, 0, 0, -5,
+                -5, 0, 0, 0, 0, 0, 0, -5,
+                -5, 0, 0, 0, 0, 0, 0, -5,
+                -5, 0, 0, 0, 0, 0, 0, -5,
+                0, 0, 0, 5, 5, 0, 0, 0
+            ],
+            chess.QUEEN: [
+                -20, -10, -10, -5, -5, -10, -10, -20,
+                -10, 0, 0, 0, 0, 0, 0, -10,
+                -10, 0, 5, 5, 5, 5, 0, -10,
+                -5, 0, 5, 5, 5, 5, 0, -5,
+                0, 0, 5, 5, 5, 5, 0, -5,
+                -10, 5, 5, 5, 5, 5, 0, -10,
+                -10, 0, 5, 0, 0, 0, 0, -10,
+                -20, -10, -10, -5, -5, -10, -10, -20
+            ],
+            chess.KING: [
+                20, 30, 10, 0, 0, 10, 30, 20,
+                20, 20, 0, 0, 0, 0, 20, 20,
+                -10, -20, -20, -20, -20, -20, -20, -10,
+                -20, -30, -30, -40, -40, -30, -30, -20,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30
+            ]
+        }
 
     def evaluate_board(self, board):
-        """
-        Evaluate the current board state from the perspective of the current player.
-        Positive score favors the current player, negative favors the opponent.
-        """
         if board.is_checkmate():
-            if board.turn:  # Black just played and checkmated white
-                return -math.inf
-            else:
-                return math.inf
+            return -math.inf if board.turn else math.inf
         if board.is_stalemate() or board.is_insufficient_material() or board.is_seventyfive_moves():
             return 0
 
         score = 0
-
-        # Material score
         for piece_type in self.piece_values:
-            score += len(board.pieces(piece_type, chess.WHITE)) * self.piece_values[piece_type]
-            score -= len(board.pieces(piece_type, chess.BLACK)) * self.piece_values[piece_type]
+            white_pieces = board.pieces(piece_type, chess.WHITE)
+            black_pieces = board.pieces(piece_type, chess.BLACK)
+            score += len(white_pieces) * self.piece_values[piece_type]
+            score -= len(black_pieces) * self.piece_values[piece_type]
 
-        # Multiply by -1 if black's turn to always evaluate from current player's perspective
-        if board.turn == chess.BLACK:
-            score = -score
+            # Piece-square tables
+            pst = self.piece_square_tables[piece_type]
+            for square in white_pieces:
+                score += pst[square]
+            for square in black_pieces:
+                score -= pst[chess.square_mirror(square)]  # mirror for black
 
-        # Add some positional evaluation (simple version)
         score += self.evaluate_pawn_structure(board)
         score += self.evaluate_mobility(board)
 
-        return score
+        return score if board.turn == chess.WHITE else -score
 
     def evaluate_pawn_structure(self, board):
-        """Evaluate pawn structure (very simplified)"""
         score = 0
-        # Doubled pawn penalty
         for file in range(8):
-            white_pawns_on_file = len(board.pieces(chess.PAWN, chess.WHITE) & chess.BB_FILES[file])
-            black_pawns_on_file = len(board.pieces(chess.PAWN, chess.BLACK) & chess.BB_FILES[file])
-
-            if white_pawns_on_file > 1:
-                score -= 10 * (white_pawns_on_file - 1)
-            if black_pawns_on_file > 1:
-                score += 10 * (black_pawns_on_file - 1)
-
+            white_pawns = len(board.pieces(chess.PAWN, chess.WHITE) & chess.BB_FILES[file])
+            black_pawns = len(board.pieces(chess.PAWN, chess.BLACK) & chess.BB_FILES[file])
+            if white_pawns > 1:
+                score -= 10 * (white_pawns - 1)
+            if black_pawns > 1:
+                score += 10 * (black_pawns - 1)
         return score if board.turn == chess.WHITE else -score
 
     def evaluate_mobility(self, board):
-        """Evaluate mobility (number of legal moves)"""
         mobility = board.legal_moves.count()
-        return mobility if board.turn == chess.WHITE else -mobility
+        return 0.1 * mobility if board.turn == chess.WHITE else -0.1 * mobility
 
-    def alpha_beta_search(self, board, depth, alpha, beta, maximizing_player):
+    def alpha_beta_search(self, board, depth, alpha, beta, maximizing):
         if depth == 0 or board.is_game_over():
             return self.evaluate_board(board)
 
-        if maximizing_player:
+        if maximizing:
             max_eval = -math.inf
             for move in board.legal_moves:
                 board.push(move)
@@ -78,7 +130,7 @@ class ChessAI:
                 max_eval = max(max_eval, eval)
                 alpha = max(alpha, eval)
                 if beta <= alpha:
-                    break  # Beta cutoff
+                    break
             return max_eval
         else:
             min_eval = math.inf
@@ -89,32 +141,25 @@ class ChessAI:
                 min_eval = min(min_eval, eval)
                 beta = min(beta, eval)
                 if beta <= alpha:
-                    break  # Alpha cutoff
+                    break
             return min_eval
 
     def find_best_move(self, board):
-        """Find the best move using alpha-beta pruning with iterative deepening"""
         best_move = None
         best_value = -math.inf if board.turn == chess.WHITE else math.inf
 
-        # Iterative deepening (optional but helps with time management)
-        for current_depth in range(1, self.depth + 1):
-            for move in board.legal_moves:
-                board.push(move)
-                if board.turn == chess.WHITE:  # We just made a move, so it's opponent's turn
-                    move_value = self.alpha_beta_search(board, current_depth - 1, -math.inf, math.inf, False)
-                else:
-                    move_value = self.alpha_beta_search(board, current_depth - 1, -math.inf, math.inf, True)
-                board.pop()
+        for move in board.legal_moves:
+            board.push(move)
+            move_value = self.alpha_beta_search(board, self.depth - 1, -math.inf, math.inf, board.turn == chess.BLACK)
+            board.pop()
 
-                if board.turn == chess.WHITE and move_value > best_value:
-                    best_value = move_value
-                    best_move = move
-                elif board.turn == chess.BLACK and move_value < best_value:
-                    best_value = move_value
-                    best_move = move
+            if board.turn == chess.WHITE and move_value > best_value:
+                best_value = move_value
+                best_move = move
+            elif board.turn == chess.BLACK and move_value < best_value:
+                best_value = move_value
+                best_move = move
 
-        # If no move found (shouldn't happen), pick random legal move
         if best_move is None:
             best_move = random.choice(list(board.legal_moves))
 
